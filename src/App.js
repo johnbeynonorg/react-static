6527eb36580c5fc7aa59340527bdc8b6c164db70
John Beynon <john@render.com>
2022-06-07 13:55:37 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        {process.env.REACT_APP_RENDER_EXTERNAL_URL}
         </p>
         <a
           className="App-link"
